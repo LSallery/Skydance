073d3ff83a7d6347b21a36a47e896c97693eb2b3
LSallery <40581662+LSallery@users.noreply.github.com>
2022-09-19 16:54:12 +0100

    Set up for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    java
+    `maven-publish`
 }
 
 group = "uk.co.sallery"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -24,4 +26,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "uk.co.sallery"
+            artifactId = "skydance"
+            version = "0.0.1"
+
+            from(components["java"])
+        }
+    }
 }
